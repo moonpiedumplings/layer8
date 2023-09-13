57a44e71a59ffc9b53dd87568cda8e66e14ec634
moonpiedumplings <jeff.fonseca777@gmail.com>
2023-09-13 14:04:14 -0700

    init

---
+++ shell.nix
@@ -0,0 +1,14 @@
+let
+    pkgs = import <nixpkgs> {};
+    quarto = pkgs.callPackage ./env/quarto.nix {};
+    python3 = pkgs.python311;
+    pythonDeps = ps: with ps; [ jupyter notebook qtconsole jupyter_console nbconvert ipykernel ipywidgets matplotlib bqplot pip ];
+in
+    pkgs.mkShell {
+        QUARTO_PYTHON = "${pkgs.python3.withPackages pythonDeps}/bin/python3";
+        packages = with pkgs; [ 
+            (python3.withPackages pythonDeps)
+            quarto
+            texlive.combined.scheme-full
+            ];
+    }
